f3e3adf54b7482e9dbb9008db3330f8a1b41210f
Naoyuki <zhaonzh@gmail.com>
2023-12-06 17:14:41 -0800

    adding code comment

--- projects/MIDIViz/src/midiviz_quick/multi_track.js
+++ projects/MIDIViz/src/midiviz_quick/multi_track.js
@@ -8,49 +8,51 @@ import NotePlayer from "../midi_player/NotePlayer.js";
 import vec2 from "../utils/Vec2.js";
 import Histogram from "../collections/Histogram.js";
 
-//a midi visualizer with Particle and NotePlayer class
+//This example provides a multitrack, multi-primitive visualizer
+//Each track is represented by a different color
+//You can bind different primitives to different tracks
 new p5(function(p5){
-    const roll = new PianoRollWithPrimitives(p5,null,true);
+    const viz = new PianoRollWithPrimitives(p5,null,true);
     const player = new NotePlayer();
     
     //set up the play settings and particle speed
     p5.setup = async function() {
         p5.createCanvas(p5.windowWidth,p5.windowHeight);
-        p5.background(255);
         const url = "../../assets/alleycat.mid"        
         await player.load(url);
+        //set play settings
         player.setSustain(0.1,0);
         player.setSustain(0.1,1);
         player.setReverb(0.5,1);
 
+        //set instrument type
         player.setInstrument("electric_piano",0);
         player.setInstrument("oboe",1);
         player.setInstrument("bass",2);
-        player.setInstrument("bass",3);
         await player.play();
 
-        roll.setSpeedScale(5e-2);
-        roll.addCollection(new QuadSet(1,5e-2,false,(detail)=>{return [240,34,12]}));
-        roll.addCollection(new ParticleSet(2,5e-2,false,(detail)=>{return [12,240,34]}));
-        roll.addCollection(new ParticleSet(0,5e-2,false,(detail)=>{return [12,34,240]}));
-        roll.addCollection(new ParticleSet(3,5e-2,false,(detail)=>{return [240,12,34]}));
+        //set global visualization unit moving speed
+        viz.setSpeedScale(5e-2);
 
-        //piano color settings
-        roll.setColorGenerator(0,(detail)=>{
+        //add primitives, and they listen to different tracks
+        viz.addCollection(new ParticleSet(0,5e-2,false,(detail)=>{return [12,34,240]}));
+        viz.addCollection(new QuadSet(1,5e-2,false,(detail)=>{return [240,34,12]}));
+        viz.addCollection(new ParticleSet(2,5e-2,false,(detail)=>{return [12,240,34]}));
+
+        //piano color settings, each track has a different color
+        viz.setColorGenerator(0,(detail)=>{
             if(detail.trackNum == 0)
                 return [12,34,240];
             else if(detail.trackNum == 1)
                 return [240,34,12];
             else if(detail.trackNum == 2)
                 return [12,240,34];
-            else if(detail.trackNum == 3)
-                return [240,12,34];
         });
 
     }
 
     //called on each frame, the draw call loop
     p5.draw = function() {
-        roll.step(p5);
+        viz.step(p5);
     }
 });
--- projects/MIDIViz/src/primitives/Primitive.js
+++ projects/MIDIViz/src/primitives/Primitive.js
@@ -1,25 +1,22 @@
 import vec2 from "../utils/Vec2.js";
 
 class Primitive{
-    //position, vec2
-    //velocity, vec2
-    //acceleration, vec2
-    //size, int
-    //color, array of int
-    //trackIdx, int
-    position;
+    //a basic primitive class, with position, velocity, acceleration, size, color, trackIdx
     velocity;
     acceleration;
+
+    position;
     size;
     color;
     trackIdx;
 
-    /** @param {vec2} position 
-     * @param {vec2} velocity
-     * @param {vec2} acceleration
-     * @param {int} size
-     * @param {int} trackIdx
-     * @param {Array<int>} color
+    /** 
+     * @param {vec2} position - the position of the primitive
+     * @param {vec2} velocity - the initial velocity of the primitive
+     * @param {vec2} acceleration - the initial acceleration of the primitive
+     * @param {int} size  - the size of the primitive
+     * @param {int} trackIdx  - the track index of the primitive
+     * @param {Array<int>} color - the color of the primitive
     */
     constructor(position, velocity, acceleration, size, trackIdx, color){
         this.position = position;
@@ -30,44 +27,87 @@ class Primitive{
         this.trackIdx = trackIdx;
     }
 
-    //important API
+    /**
+     * @description - called every frame to update the position of the primitive
+     * @params no parameters
+     * @returns no return value
+     */
     advance(){
         this.velocity = this.velocity.add(this.acceleration);
         this.position = this.position.add(this.velocity);
     }
 
+    /**
+     * @description - called every frame to check if the primitive is out of the boundary
+     * @param {p5} p5 - a p5.js instance
+     * @returns {boolean} - true if the primitive is out of the boundary, false otherwise
+     */
     checkBoundary(p5){
         if(this.position.x<0 || this.position.x>p5.windowWidth || this.position.y<0 || this.position.y>p5.windowHeight)
             return true;
         return false;
     }
 
+    /**
+     * @description - called every frame to draw the primitive
+     * @param {p5} p5 - a p5.js instance
+     * @returns no return value
+     */
     draw(p5){
         p5.fill(this.color);
         p5.noStroke();
     }
 
-    //setters
+    /**
+    * @description - set the color of the primitive
+    * @param {Array<int>} color - the color of the primitive in RGB, e.g. [0,0,0] is black
+    * @returns no return value
+     */
     setColor(color){
         this.color = color;
     }
 
+    /**
+     * @description - set the track index of the primitive
+     * @param {int} trackIdx - the track index of the primitive
+     * @returns no return value
+     */
     setTrackIdx(trackIdx){
         this.trackIdx = trackIdx;
     }
 
+    /**
+     * @description - set the acceleration of the primitive
+     * @param {vec2} acceleration - the acceleration of the primitive
+     * @returns no return value
+     */
     setAcceleration(acceleration){
         this.acceleration = acceleration;
     }
 
+    /**
+     * @description - set the velocity of the primitive
+     * @param {vec2} velocity - the velocity of the primitive
+     * @returns no return value
+     */
     setVelocity(velocity){
         this.velocity = velocity;
     }
 
+    /**
+     * @description - set the position of the primitive
+     * @param {vec2} position - the position of the primitive in pixel
+     * @returns no return value
+     */
     setPosition(position){
         this.position = position;
     }
 
+    /**
+     * @description - set the size of the primitive
+     * @param {int} size - the size of the primitive in pixels
+     * @returns no return value
+    */
     setSize(size){
         this.size = size;
     }
